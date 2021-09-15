db86208886b38121da04aeb54699cab795a1472d
Rocio Jazmin Sanchez Suarez <rocioj433@gmail.com>
2021-09-15 10:49:22 -0300

    Add files via upload

---
+++ clase 11/app.js
@@ -0,0 +1,10 @@
+alert("Bienvenide! :)");
+function primerBoton(){
+    alert("hicise click en el primer boton");
+}
+function segundoBoton(){
+    window.confirm("Te gusta js?");
+}
+function tercerBoton(){
+    window.prompt("Como es tu nombre?");
+}
