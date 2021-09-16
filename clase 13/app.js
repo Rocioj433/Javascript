c137ef6cdffa73ce2bdc0f9beb986c31fcac0279
Rocio Jazmin Sanchez Suarez <rocioj433@gmail.com>
2021-09-16 15:22:40 -0300

    Add files via upload

---
+++ clase 13/app.js
@@ -0,0 +1,38 @@
+// variable
+let miSegundaVariable; //local
+ //mixto
+//la vieja forma
+var esto_Es_Una_Variable;
+var miPrimerVariable;
+var numerosEnteros;
+var numerosenteros;
+function scoper(){
+    let prueba;
+}
+//suma
+var primerNumero = 2;
+var segundoNumero = 3;
+var resultado = primerNumero + segundoNumero;
+console.log("Tu resultado es: "+resultado);
+// nombre
+var primerNombre = "Rocio";
+var segundoNombre = "Jazmin";
+var nombreCompleto = primerNombre + segundoNumero;
+console.log(nombreCompleto);
+// tipos de  datos
+/*
+Numericos: INT(enteros) o FLOAT(flotanter)
+ej: 1   1.5
+
+Texto: STRING(alfanumerica) ="x" <- es un texto
+Operadore aritmeticos: + = suma, - = resta, / = division, * = multiplicacion
+Operadores extendidos: % = resto, %% = modulo, 
+Potencias: math.logpow para potencias
+ej: console.log(math.pow(7,3)); se puede poner una variable declarada
+console,log(Mathe.round(1.77777777));
+console.log(math.random()*1000);
+math.round(math.random()*1000);
+function numAleatorioVec(min,max){
+    console.log(Math.random()*(max-min) + (min));
+}
+*/
